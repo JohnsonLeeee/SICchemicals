e55c3a23946fbf07f7fc3160ca9f8847d1d94eee
JohnsonLeeee <1757670451@qq.com>
2018-05-15 08:53:09 +0800

    注册了几个数据库，修改了urls，重置了views
    Signed-off-by: JohnsonLeeee <1757670451@qq.com>

--- SICchemicals/chemicals/admin.py
+++ SICchemicals/chemicals/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Chemical
+from .models import Chemical, Person, Staff, Student, Location, ChemicalsMessage
 
-admin.site.register(Chemical)
+admin.site.register(Chemical, Person, Staff, Student, Location, ChemicalsMessage)
--- SICchemicals/chemicals/views.py
+++ SICchemicals/chemicals/views.py
@@ -7,19 +7,3 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('欢迎登陆碳化物课题组药品管理系统')
 
-
-def hello(request):  # 功能选择
-    # t = loader.get_template("hello.html")
-    # c = RequestContext(request)
-    return render_to_response("hello.html", context_instance=RequestContext(request))
-    return render(request, "hello.html", )
-
-def addstu(request):# 添加学生信息
-  name = request.POST.get("name")
-  age = request.POST.get("age")
-  learn = request.POST.get("learn")
-  newstu = Student(name=name,age=age,learn=learn)
-  newstu.save()
-  # c = RequestContext(request)
-  # t = loader.get_template("check.html")
-  return render_to_response("done.html",context_instance=RequestContext(request,{"age":age,"learn":learn,"name":name}))
