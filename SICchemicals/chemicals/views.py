7fa46ba2b2404c858250d34547dab9c4ddbf35ba
JohnsonLeeee <1757670451@qq.com>
2018-06-23 00:13:40 +0800

    添加了首页重定向至药品管理页面 Signed-off-by: JohnsonLeeee <1757670451@qq.com>

--- SICchemicals/chemicals/views.py
+++ SICchemicals/chemicals/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('欢迎登陆碳化物课题组药品管理系统')
+    username = "visitor"
+    password = "123456"
+    user = authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        login(request, user)
+        return HttpResponseRedirect("admin/chemicals/chemical/")
+    else:
+        return HttpResponse(u"登录失败，请联系李帅13122386386")
+
+
+
+
 
